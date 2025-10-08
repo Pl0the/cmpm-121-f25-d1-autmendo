55b02ba24ca3bf78ffcb7df3ae72bd465e56c535
Pl0the <eclap9145@gmail.com>
2025-10-08 15:39:37 +0000

    Added button logic to increase counter and added div element (step 2)
    Co-authored-by: slashleyonline <slashleyonline@users.noreply.github.com>

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,19 @@
 const button = document.createElement("button");
-button.textContent = "💀";
+button.textContent = "Click me!!!";
 button.style.fontSize = "8em";
 document.body.appendChild(button);
+
+let counter = 0;
+
+const counterText = document.createElement("div");
+counterText.style.fontSize = "8em";
+counterText.innerText = 'clicks: 0'
+document.body.appendChild(counterText);
+
+
+
+button.addEventListener('click',() => {
+  counter += 1;
+  counterText.innerText = 'clicks: ' + counter 
+  console.log("button clicked!")
+});
