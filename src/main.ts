afdf377b9aa93e8cbec12191b7b35581bb078508
Pl0the <eclap9145@gmail.com>
2025-10-08 16:21:11 +0000

    Fixed formatting, look of game, and changed some logic (Step 2)

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,21 @@
 const button = document.createElement("button");
-button.textContent = "Click me!!!";
+button.textContent = "💀";
 button.style.fontSize = "8em";
+button.style.padding = "0.2em 0.4em";
+button.style.borderRadius = "0.2em";
+button.style.marginLeft = "40px";
 document.body.appendChild(button);
 
-let counter = 0;
+let counter: number = 0;
 
 const counterText = document.createElement("div");
-counterText.style.fontSize = "8em";
-counterText.innerText = "clicks: 0";
+counterText.style.fontSize = "3em";
+counterText.innerText = "Click to gain Skulls";
 document.body.appendChild(counterText);
 
 button.addEventListener("click", () => {
   counter += 1;
-  counterText.innerText = "clicks: " + counter;
-  console.log("button clicked!");
+  counterText.style.fontSize = "4em";
+  counterText.style.marginLeft = "80px";
+  counterText.innerText = "Skulls " + counter;
 });
