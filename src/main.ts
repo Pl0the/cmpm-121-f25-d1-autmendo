a7b7e6aa7287677afd6fd9db1cf3426d7504293b
Pl0the <eclap9145@gmail.com>
2025-10-08 03:57:04 +0000

    added button clicking logic (step 1)

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,4 @@
-import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
-import "./style.css";
-
-document.body.innerHTML = `
-  <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
-`;
+const button = document.createElement("button");
+button.textContent = "💀";
+button.style.fontSize = "8em";
+document.body.appendChild(button);
