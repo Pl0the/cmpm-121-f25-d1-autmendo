ba2835f12708f959c5fb7b8e2cff11ffe1bbf3f2
Pl0the <eclap9145@gmail.com>
2025-10-10 19:06:51 +0000

    added continous growth to automatic button, updated logic to show souls per second correctly (Step 4)

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,8 @@ let counter: number = 0;
 
 let automaticCounter: number = 0;
 
+let SoulsPS: number = 0;
+
 const counterText = document.createElement("div");
 counterText.style.fontSize = "3em";
 counterText.innerText = "Click to reap souls";
@@ -23,15 +25,6 @@ button.addEventListener("click", () => {
   counterText.innerText = "Souls: " + counter;
 });
 
-function autoClick() {
-  counter += 1;
-  counterText.innerText = "Souls: " + counter;
-  counterText.style.marginLeft = "240px";
-  counterText.style.fontSize = "4em";
-  automaticButton.innerText = "Souls per second: " + automaticCounter;
-  automaticButton.style.marginLeft = "180px";
-}
-
 const automaticButton = document.createElement("button");
 automaticButton.innerText = "Cick to automatically reap souls";
 automaticButton.style.fontSize = "2em";
@@ -41,6 +34,23 @@ automaticButton.style.marginLeft = "100px";
 document.body.appendChild(automaticButton);
 
 automaticButton.addEventListener("click", () => {
-  setInterval(autoClick, 1000);
   automaticCounter += 1;
+  const currentRate = automaticCounter;
+  SoulsPS += currentRate;
+
+  let lastTime = performance.now();
+
+  function autoClick(currentTime: number) {
+    const elapsed = (currentTime - lastTime) / 1000;
+    lastTime = currentTime;
+    counter += elapsed * currentRate;
+    counterText.innerText = "Souls: " + counter.toFixed(2);
+    counterText.style.marginLeft = "220px";
+    counterText.style.fontSize = "4em";
+    automaticButton.innerText = "Souls per second: " + SoulsPS.toFixed(0);
+    automaticButton.style.marginLeft = "180px";
+    requestAnimationFrame(autoClick);
+  }
+
+  requestAnimationFrame(autoClick);
 });
